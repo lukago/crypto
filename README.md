62c681afcd3fe85e7127ae8f7c0e8a769d530c76
lukago <golebiewski96@gmail.com>
2017-11-21 14:54:46 +0100

    elgamal first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# kryptografia
-Podstawy Kryptografii
---
+++ src/main/java/krypto/elgamal/App.java
@@ -0,0 +1,8 @@
+package krypto.elgamal;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
