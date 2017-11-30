74693a5b512ba2a33d3065ac448a6169217e7af3
lukago <golebiewski96@gmail.com>
2017-11-30 22:09:03 +0100

    encr, decr, io

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <cstring>
 
 #include "BigInt.h"
 #include "primes.h"
@@ -85,19 +90,208 @@ pair<PrivateKey, PublicKey> generateKeys(int bits, int confidence)
     return make_pair(priv, pub);
 }
 
-int main()
+vector<BigInt> encode(const string &plainText, int bits)
 {
-    srand(time(0));
+    int charBits = 8;
+    int packets = bits / (charBits + 1);
+    vector<char> bytes(plainText.begin(), plainText.end());
+    vector<BigInt> encoded;
 
-    auto res = generateKeys(1024, 5);
+    int index = -1;
+    for (int i = 0; i < bytes.size(); i++) {
+        if (i % packets == 0) {
+            index++;
+            encoded.emplace_back(0);
+        }
 
-    cout << res.first.p << endl;
-    cout << res.first.g << endl;
-    cout << res.first.x << endl << endl;
+        encoded[index] += fastPow(2, charBits * (i % packets)) * (bytes[i] & 0xFF);
+    }
 
-    cout << res.second.p << endl;
-    cout << res.second.g << endl;
-    cout << res.second.h << endl;
+    return encoded;
+}
 
+string decode(const vector<BigInt> &encodedText, int bits)
+{
+    int charBits = 8;
+    int packets = bits / (charBits + 1);
+    vector<char> bytes;
+
+    for (BigInt num : encodedText) {
+        for (int j = 0; j < packets; j++) {
+            BigInt temp = num;
+
+            for (int k = j + 1; k < packets; k++)
+                temp = temp % fastPow(2, charBits * k);
+
+            BigInt letter = temp / fastPow(2, charBits * j);
+            bytes.push_back((char) letter.longValue());
+            num -= letter * fastPow(2, charBits * j);
+        }
+    }
+
+    vector<char> bytesF;
+    copy_if(bytes.begin(), bytes.end(), back_inserter(bytesF), [](char i) {
+        return i != 0;
+    });
+
+    return string(bytesF.begin(), bytesF.end());
+}
+
+string encrypt(const string &plainText, PublicKey key)
+{
+    vector<BigInt> z = encode(plainText, key.bits);
+
+    BigInt y, c, s, d;
+    string encryptedStr;
+    for (const BigInt &i : z) {
+        y = xrand(1, key.p);
+        c = modPow(key.g, y, key.p);
+        s = modPow(key.h, y, key.p);
+        d = (i * s) % key.p;
+        encryptedStr.append(c.toString() + " " + d.toString() + " ");
+    }
+
+    return encryptedStr;
+}
+
+string decrypt(const string &cipher, PrivateKey key)
+{
+    vector<BigInt> encodedText;
+    BigInt c, d, s, srev, plain;
+    stringstream ss(cipher);
+    vector<string> cipherVec{istream_iterator<string>{ss}, istream_iterator<string>{}};
+
+    for (int i = 0; i < cipherVec.size(); i += 2) {
+        c = BigInt(cipherVec[i]);
+        d = BigInt(cipherVec[i + 1]);
+        s = modPow(c, key.x, key.p);
+        srev = modPow(s, key.p - 2, key.p);
+        plain = (d * srev) % key.p;
+        encodedText.push_back(plain);
+    }
+
+    return decode(encodedText, key.bits);
+}
+
+// input/output utils
+void saveToFile(const vector<string> &data, const string &fileName);
+void saveMsgToFile(const string &msg, const string &fileName);
+vector<string> readFile(const string &fileName);
+string readMsg(const string &fileName);
+vector<string> privKeyToVec(const PrivateKey &key);
+vector<string> pubKeyToVec(const PublicKey &key);
+PrivateKey privKeyFromVec(const vector<string> &data);
+
+// ====================================================================
+// =============================== MAIN ===============================
+// ====================================================================
+int main(int argc, char **argv)
+{
+    srand(time(nullptr));
+
+    if (argc > 0 && strcmp(argv[1], "encr") == 0) {
+        string msgPath = argv[2];
+        int bits = stoi(argv[3]);
+        int conf = stoi(argv[4]);
+        string pubKeyPath = argv[5];
+        string privKeyPath = argv[6];
+        string cipherPath = argv[7];
+
+        auto keys = generateKeys(bits, conf);
+        string cipher = encrypt(readMsg(msgPath), keys.second);
+
+        saveToFile(pubKeyToVec(keys.second), pubKeyPath);
+        saveToFile(privKeyToVec(keys.first), privKeyPath);
+        saveMsgToFile(cipher, cipherPath);
+
+        cout << "Encrypted.";
+        return 0;
+    }
+
+    if (argc > 0 && strcmp(argv[1], "decr") == 0) {
+        string cipherPath = argv[2];
+        string privKeyPath = argv[3];
+        string decmsgPath = argv[4];
+
+        PrivateKey privateKey = privKeyFromVec(readFile(privKeyPath));
+        string msg = decrypt(readMsg(cipherPath), privateKey);
+
+        saveMsgToFile(msg, decmsgPath);
+
+        cout << "Decrypted.";
+        return 0;
+    }
+
+    string usage = "Usage:\n"
+            "encr {path to msg} {prime bits num} {prime test confidence} "
+            "{path to save pub key} {path to save priv key} {path to save cipher}\n"
+            "decr {path to cipher} {priv key path} {path to save decrypted message}";
+
+    cout << usage;
     return 0;
+}
+
+void saveToFile(const vector<string> &data, const string &fileName)
+{
+    ofstream out(fileName);
+    for (const auto &i : data) out << hex << i << "\n";
+    out.close();
+}
+
+void saveMsgToFile(const string &msg, const string &fileName)
+{
+    ofstream out(fileName);
+    out << hex << msg;
+    out.close();
+}
+
+vector<string> readFile(const string &fileName)
+{
+    string line;
+    ifstream in(fileName);
+    vector<string> data;
+    while (getline(in, line))
+        data.push_back(line);
+    in.close();
+    return data;
+}
+
+string readMsg(const string &fileName)
+{
+    ifstream in(fileName);
+    std::stringstream buffer;
+    buffer << in.rdbuf();
+    string msg = buffer.str();
+    in.close();
+    return msg;
+}
+
+vector<string> privKeyToVec(const PrivateKey &key)
+{
+    return vector<string>{
+            key.p.toString(),
+            key.g.toString(),
+            key.x.toString(),
+            to_string(key.bits)
+    };
+}
+
+vector<string> pubKeyToVec(const PublicKey &key)
+{
+    return vector<string>{
+            key.p.toString(),
+            key.g.toString(),
+            key.h.toString(),
+            to_string(key.bits)
+    };
+}
+
+PrivateKey privKeyFromVec(const vector<string> &data)
+{
+    return PrivateKey{
+            BigInt(data[0]),
+            BigInt(data[1]),
+            BigInt(data[2]),
+            stoi(data[3])
+    };
 }
